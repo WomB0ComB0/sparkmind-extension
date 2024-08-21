ad521b1b1130a3e33534421adc8178e9ea394549
Mike Odnis <airwolf635@gmail.com>
2024-08-21 01:35:08 -0400

    chore: gitignore cachce

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,28 +1,30 @@
-import { useEffect, useState } from "react"
 import { Link } from "@nextui-org/link"
+import { useEffect, useState } from "react"
+
 import { Button } from "~components/ui/button"
+
 import "~styles/style.css"
 
 function IndexPopup() {
   const [url, setUrl] = useState<string>("")
   useEffect(() => {
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    })
-    .then((url) => {
-      console.log(url)
-      console.log(url[0].url)
-      setUrl(url[0].url)
-    })
-    .catch((error) => console.error(error))
+    chrome.tabs
+      .query({
+        active: true,
+        currentWindow: true
+      })
+      .then((url) => {
+        console.log(url)
+        console.log(url[0].url)
+        setUrl(url[0].url)
+      })
+      .catch((error) => console.error(error))
   }, [])
   return (
     <div
       style={{
         padding: 16
-      }}
-    >
+      }}>
       <h2 className="text-2xl font-bold text-red-500">
         Welcome to your{" "}
         <Link href="https://www.plasmo.com" target="_blank" rel="noreferrer">
@@ -34,13 +36,10 @@ function IndexPopup() {
         href="https://docs.plasmo.com"
         target="_blank"
         rel="noreferrer"
-        className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md"
-      >
+        className="px-4 py-2 mt-4 text-white bg-blue-500 rounded-md">
         View Docs
       </Link>
-      <p>
-        {url}
-      </p>
+      <p>{url}</p>
       <Button>Click me</Button>
     </div>
   )
